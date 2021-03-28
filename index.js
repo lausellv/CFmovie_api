d59e832d1f8e3252807de1d3ea22c69af9ec818c
lausellv <lausellv@yahoo.com>
2021-03-28 19:24:20 +0200

    No longer need this variable

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const express = require('express'),
   morgan = require('morgan'),
-  bodyParser = require('body-parser'),
-  app = express(),
   uuid = require('uuid'),
   mongoose = require('mongoose'),
-  Models = require('./models.js');
-
+  Models = require('./models.js'),
+  app = express(),
+  bodyParser = require('body-parser');
 
 const Movies = Models.Movie;
 const Users = Models.User;
@@ -43,80 +42,84 @@ app.get('/', (req, res) => {
 
 // Gets the list of data about ALL movies
 app.get('/movies', (req, res) => {
-  res.json(movies);
+  Movies.find()
+  .then((movies) => {
+    res.status(200).json(movies);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send('Error: ' + err);
+  });
 });
 
 //get data about a single movie by title
 app.get('/movies/:title', (req, res) => {
-  res.json(
-    movies.find((movie) => {
-      return movie.title === req.params.title;
-    })
-  );
+  Movies.findOne({ Title: req.params.title })
+  .then((movie) => {
+    res.json(movie);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send('Error: ' + err);
+  });
+
 });
 
-// get a movie by id
-app.get('/movies?id=1', (req, res) => {
-  res.json(
-    movies.find((movie) => {
-      return movie.id == req.params.id;
-    })
-  );
-  res.json(movies.filter((movie) => movie.id === parseInt(req.params.id)));
-});
+
 
 // send documentation file
 app.get('./documentation', (req, res) => {
   res.sendFile('documentation.html');
 });
 
-// //  Renders documentation page
-// app.use('/documentation', express.static('public'));
 
-//Add data for a new movie to the movie list
-app.post('/movies', (req, res) => {
-  let newMovie = req.body;
-
-  if (!newMovie.title) {
-    const message = 'Missing movie "title" in request body';
-    res.status(400).send(message);
-  } else {
-    newMovie.id = uuid.v4();
-    movies.push(newMovie);
-    res.status(201).send(newMovie);
-  }
-});
 
 //delete a movie by title
 app.delete('/movies/:title', (req, res) => {
-  let movie = movies.find((movie) => {
-    return movie.title === req.params.title;
+  Movies.findOne({ 'Title': req.params.title })
+  .then((movies) => {
+    res.json(movies);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send('Error: ' + err);
   });
-  if (movie) {
-    movies = movies.filter((obj) => {
-      return obj.id !== req.params.id;
-    });
-    res.status(201).send(`${req.params.title} was deleted.`);
-  }
 });
 
-//get data about a single movie by director
-app.get('/movies/directors/:director', (req, res) => {
-  res.json(
-    movies.find((movie) => {
-      return movie.director === req.params.director;
-    })
-  );
+//GET movies by director (using his name in the query)
+// app.get('/movies/directors/:director', (req, res) => {
+//   Movies.find({ 'Director.Name': req.params.director })
+//   .then((movies) => {
+//     res.json(movies);
+//   })
+//   .catch((err) => {
+//     console.error(err);
+//     res.status(500).send('Error: ' + err);
+//   });
+// });
+
+//GET data about a director by their name (using his name in the query)
+app.get('/movies/directors/:name', (req, res) => {
+  Movies.findOne({'Director.Name': req.params.name})
+      .then((director) => {
+        res.json(director.Director);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+      });
 });
 
-//get data about a single movie by genre
+//GET movies by genre
 app.get('/movies/genres/:genre', (req, res) => {
-  let filteredMovies = movies.filter((movie) => {
-    return movie.genre === req.params.genre;
+  Movies.find({ 'Genre.Name': req.params.genre })
+  .then((movies) => {
+    res.json(movies);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send('Error: ' + err);
   });
-
-  console.log(filteredMovies, '======');
-  res.status(200).send({ movies: filteredMovies });
 });
 
 // update a user
@@ -140,23 +143,56 @@ app.put('/users/:username', (req, res) => {
   });
 });
 
-//get all users
+//GET a list of all users
 app.get('/users', (req, res) => {
-  res.json(users);
+  Users.find()
+    .then((users) => {
+      res.status(200).json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
 });
 
-//Add a new user
-app.post('/users', (req, res) => {
-  let newUser = req.body;
+// get a user by username using Mongoose
+app.get('/users/:Username', (req, res) => {
+  Users.findOne({ Username: req.params.Username })
+    .then((user) => {
+      res.json(user);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
 
-  if (!newUser.username) {
-    const message = 'Missing "name" in request body';
-    res.status(400).send(message);
-  } else {
-    newUser.id = uuid.v4();
-    users.push(newUser);
-    res.status(201).send(newUser);
-  }
+//ADD a new user
+app.post('/users', (req, res) => {
+  Users.findOne({ Username: req.body.Username })
+    .then((user) => {
+      if (user) {
+        return res.status(400).send(req.body.Username + 'already exists');
+      } else {
+        Users.create({
+          Username: req.body.Username,
+          Password: req.body.Password,
+          Email: req.body.Email,
+          Birthday: req.body.Birthday,
+        })
+          .then((user) => {
+            res.status(201).json(user);
+          })
+          .catch((error) => {
+            console.error(error);
+            res.status(500).send('Error: ' + error);
+          });
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Error: ' + error);
+    });
 });
 
 //delete a user
@@ -176,92 +212,36 @@ app.delete('/users/:username', (req, res) => {
 
 app.listen(5000, () => console.log(`App is listening on port 5000`));
 
-// object variable with top movies
-let movies = [
-  {
-    id: 1,
-    title: 'Planet Earth II',
-    description:
-      "Wildlife documentary series with David Attenborough, beginning with a look at the remote islands which offer sanctuary to some of the planet's rarest creatures, to the beauty of cities, which are home to humans, and animals..",
-    genre: 'Documentary',
-    director: 'David Attenborough',
-    year: 2016,
-  },
-  {
-    id: 2,
-    title: 'Seabiscuit',
-    description:
-      'True story of the undersized Depression-era racehorse whose victories lifted not only the spirits of the team behind it but also those of their nation.',
-    genre: ['Drama', 'History', 'Sport'],
-    director: 'Gary Ross',
-    year: 2003,
-  },
-  {
-    id: 3,
-    title: 'If Beale Street Could Talk',
-    description:
-      "A young woman embraces her pregnancy while she and her family set out to prove her childhood friend and lover innocent of a crime he didn't commit.",
-    genre: 'Drama',
-    director: 'Barry Jenkins',
-    year: 2018,
-  },
-  {
-    id: 4,
-    title: 'The Greatest Showman',
-    description:
-      'P T Barnum becomes a worldwide sensation in the show business. His imagination and innovative ideas take him to the top of his game.',
-    genre: 'Drama',
-    director: 'Michael Gracey',
-    year: 2017,
-  },
-  {
-    id: 5,
-    title: 'Crazy Rich Asians',
-    description:
-      'Rachel, a professor, dates a man named Nick and looks forward to meeting his family. However, she is shaken up when she learns that Nick belongs to one of the richest families in the country.',
-    genre: 'Romance',
-    director: 'Jon M. Chu',
-    year: 2018,
-  },
-  {
-    id: 6,
-    title: 'Joker',
-    description:
-      'Arthur Fleck, a party clown, leads an impoverished life with his ailing mother. However, when society shuns him and brands him as a freak, he decides to embrace the life of crime and chaos.',
-    genre: 'Thriller',
-    director: 'Todd Phillips',
-    year: 2019,
-  },
-  {
-    id: 7,
-    title: 'Knives Out',
-    description:
-      'The circumstances surrounding the death of crime novelist Harlan Thrombey are mysterious, but there is one thing that renowned Detective Benoit Blanc knows for sure - everyone in the wildly dysfunctional Thrombey family is a suspect.',
-    genre: 'Mystery',
-    director: 'Rian Johnson',
-    image: 'knivesOut.png',
-    year: 2019,
-  },
-  {
-    id: 8,
-    title: 'The Great Gatsby',
-    description:
-      'Nick Carraway, a World War I veteran who moves to New York with the hope of making it big, finds himself attracted to Jay Gatsby and his flamboyant lifestyle.',
-    genre: 'Drama',
-    director: 'Baz Luhrmann',
-    year: 2013,
-  },
-];
+
+
 
 //object used to test the POST request on Postman
 let addtlMovie = {
-  title: 'The Notebook',
-  description:
-    'A poor yet passionate young man falls in love with a rich young woman, giving her a sense of freedom, but they are soon separated because of their social differences.',
-  genre: 'Drama',
-  director: 'Nick Cassavetes',
-  year: 2004,
+  
+  "title": "The Notebook",
+  "description": "A poor yet passionate young man falls in love with a rich young woman, giving her a sense of freedom, but they are soon separated because of their social differences.",
+  "genre": "Drama",
+  "director": "Nick Cassavetes",
+  "year": 2004
+
 };
+
+// {
+//   "Genre": {
+//       "Name": "Drama",
+//       "Description": "Nick Cassavetes was born in New York City, the son of actress Gena Rowlands and Greek-American actor and film director John Cassavetes."
+//   },
+//   "Director": {
+//       "Name": "Nick Cassavetes",
+//       "Bio": "",
+//       "Birth": 1959
+//   },
+//   "Actors": [],
+//   "Title": "The Notebook",
+//   "Description": "A poor yet passionate young man falls in love with a rich young woman, giving her a sense of freedom, but they are soon separated because of their social differences.",
+//   "ImageURL": "https://m.media-amazon.com/images/M/MV5BNDUwMDUyMDAyNF5BMl5BanBnXkFtZTYwMDQ3NzM3._V1_UX182_CR0,0,182,268_AL_.jpg",
+//   "Featured": false
+// }
 
 // users
 let users = [
